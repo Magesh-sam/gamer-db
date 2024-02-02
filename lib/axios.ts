a485fa39de4697128c5d6e51719def476440721c
mageshkannan <mageshkannanam@gmail.com>
2024-02-02 18:25:37 +0530

    feat: initial setup

---
+++ lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+const NEXT_PUBLIC_RAWG_API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+
+const RAWG = axios.create({
+  baseURL: "https://api.rawg.io/api/",
+  params: {
+    key: NEXT_PUBLIC_RAWG_API_KEY,
+  },
+});
+
+export default RAWG;
