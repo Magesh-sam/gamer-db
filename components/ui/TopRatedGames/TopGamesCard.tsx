1a1bc0effb95eb62eb60e75f732b5fe1198ac686
mageshkannan <mageshkannanam@gmail.com>
2024-02-03 20:20:43 +0530

    fix: image size - carousel

--- components/ui/TopRatedGames/TopGamesCard.tsx
+++ components/ui/TopRatedGames/TopGamesCard.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function TopGamesCard({name,img}:TopGamesCardProps) {
   return (
     <div className='relative rounded-md  h-48 sm:h-60 md:h-80 lg:h-[400px] '>
-      <Image src={img} alt={name} fill className='w-full h-auto object-contain object-center ' />
+      <Image src={img} alt={name} fill className='w-full h-auto object-fill object-center ' />
       <div className='absolute bottom-0 left-0 right-0 px-3 py-1    backdrop-blur-md drop-shadow-lg w-full'>
         <h3 className='text-white font-bold'>{name}</h3>
       </div>
