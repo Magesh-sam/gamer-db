71a39cb1e9b4fb8048760da5f5d4caa4f877e537
mageshkannan <mageshkannanam@gmail.com>
2024-02-04 10:20:20 +0530

    husky working

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ async function page() {
         <h2 className="text-2xl font-semibold mb-4 f  irst-letter:text-primary">
           Top Rated Games
         </h2>
-                              <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
           <TopGamesCarousel games={topRatedGames} />
         </Suspense>
       </section>
